18bd290415c6ee4d958b51afc0065e0fdccf3330
Joel Romero <joelrg1288@gmail.com>
2019-06-07 12:41:14 -0500

    Inscribir taller

--- taller.php
+++ taller.php
@@ -27,19 +27,54 @@ if ($_GET["lang"]) {
                 <br>
                 <h2>Inscribir Taller</h2>
                 <br>
-                <button type="button" class="btn btn-primary inscribir-taller-btn m-b" data-toggle="modal" data-target="#inscribirTaller">Inscribir un taller</button>
+                <button type="button" class="btn btn-primary inscribir-taller-btn m-b" data-toggle="modal" data-target="#inscribir-taller">Inscribir un taller</button>
                 <br>
                 <br>
                 <br>
                 <br>
-                <div id="sinTaller">
+                <div id="sin-taller" style="display: none">
                   <h3>Aún no te has inscrito a ningún taller</h3>
                 </div>
+                  <div class="row">
+                    <div class="col-lg-6 col-lg-offset-1 features-text">
+                      <small>Taller</small>
+                    </div>
+                    <div class="col-lg-2 features-text">
+                      <small>Estatus</small>
+                    </div>
+                    <div class="col-lg-2 features-text">
+                      <small>Cancelar</small>
+                    </div>
+                  </div>
+                  <?php
+                  $talleres = array("Introducción al desarrollo de apps móviles con realidad aumentada", "Big Data en la ciencia de datos", "Entrega de aplicaciónes con Docker");
+                  $estatus = array("Pendiente", "Verificado", "Pendiente");
+                  for ($i=0; $i < count($talleres); $i++) { 
+                    echo "<div class="."'row'".">";
+                    echo "<div class="."'col-lg-6 col-lg-offset-1 features-text'".">";
+                    echo "<h4>".$talleres[$i]."</h4></div>";
+                    echo "<div class="."'col-lg-2 features-text'".">";
+                    echo "<h5>".$estatus[$i]."</h5></div>";
+                    echo "<div class="."'col-lg-2 features-text'".">";
+                    echo "<button type="."button".">";
+                    echo "<i class="."'fa fa-close'"."></i></button></div>";
+                    echo "</div>";
+                  }
+                  ?>
+                  <!--<div class="row">
+                    <div class="col-lg-6 col-lg-offset-1 features-text">
+                      <h4>Microarquitectura del procesador Lagarto</h4></div>
+                    <div class="col-lg-2 features-text">
+                      <h5>Pendiente</h5></div>
+                    <div class="col-lg-2 features-text">
+                      <button type="button">
+                        <i class="fa fa-close"></i></button></div>
+                  </div>-->
             </div>
         </div>
         <br>
         <br>
-        <div class="modal fade" id="inscribirTaller" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal fade" id="inscribir-taller" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <form class="m-t" role="form" method="post" action="taller">
@@ -72,7 +107,7 @@ if ($_GET["lang"]) {
         <script src="js/plugins/iCheck/icheck.min.js"></script>
         <script src="js/plugins/toastr/toastr.min.js"></script>
         <script>
-            $('#inscribirTaller').on('show.bs.modal', function() {
+            $('#inscribir-taller').on('show.bs.modal', function() {
               $('.i-checks').iCheck({
                 checkboxClass: 'icheckbox_square-green',
                 radioClass: 'iradio_square-green',
